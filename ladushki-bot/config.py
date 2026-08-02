22355d950339dfe625a7cef6aaabb43566959e29
Ladushka008 <sewrgthhjkk@inbox.lv>
2026-08-02 10:55:26 +0300

    Add configuration class for bot settings

--- ladushki-bot/config.py
+++ ladushki-bot/config.py
@@ -1,1 +1,13 @@
+import os
 
+
+class Config:
+    TOKEN: str = os.getenv("BOT_TOKEN", "")
+    ADMIN_ID: int = int(os.getenv("ADMIN_ID", "7837011810"))
+    PORT: int = int(os.getenv("PORT", 8080))
+    GROUP_CHAT_ID: str = os.getenv("GROUP_CHAT_ID", "@ladushka09")
+    DB_FILE: str = "database.db"
+
+
+if not Config.TOKEN:
+    raise ValueError("Переменная окружения BOT_TOKEN не установлена!")
