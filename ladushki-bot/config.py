6dbc1329a6d9471abac5e961ceb2baedb1f7d19e
Ladushka008 <sewrgthhjkk@inbox.lv>
2026-08-02 11:42:37 +0300

    Update config.py

--- ladushki-bot/config.py
+++ ladushki-bot/config.py
@@ -8,6 +8,12 @@ class Config:
     GROUP_CHAT_ID: str = os.getenv("GROUP_CHAT_ID", "@ladushka09")
     DB_FILE: str = "database.db"
 
+    # Параметры резервного копирования в GitHub
+    GITHUB_TOKEN: str = os.getenv("GITHUB_TOKEN", "")
+    GITHUB_OWNER: str = os.getenv("GITHUB_OWNER", "")
+    GITHUB_REPO: str = os.getenv("GITHUB_REPO", "")
+    GITHUB_DB_PATH: str = os.getenv("GITHUB_DB_PATH", "database.db")
+
 
 if not Config.TOKEN:
     raise ValueError("Переменная окружения BOT_TOKEN не установлена!")
