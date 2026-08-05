bcac34b57ed5d99a04da3494b28685b967a58c2d
Ladushka008 <sewrgthhjkk@inbox.lv>
2026-08-05 20:20:02 +0300

    Update config.py

--- ladushki-bot/config.py
+++ ladushki-bot/config.py
@@ -3,6 +3,7 @@ import os
 
 class Config:
     TOKEN: str = os.getenv("BOT_TOKEN", "")
+    ADMIN_ID: int = int(os.getenv("ADMIN_ID", "7837011810")) # <--- Добавьте эту строку
     GROUP_CHAT_ID: str = os.getenv("GROUP_CHAT_ID", "@ladushka09")
     PORT: int = int(os.getenv("PORT", 8080))
 
